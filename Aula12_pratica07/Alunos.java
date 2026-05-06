c19481e27540ca0e2e7f5e9098ac098a9f66e9a2
pedrogabrielfdev <pedro027329@gmail.com>
2026-05-06 11:32:44 -0300

    tirei as piadas
    codigo ta certo agora

--- Aula12_pratica07/Alunos.java
+++ Aula12_pratica07/Alunos.java
@@ -1,8 +1,8 @@
 public class Alunos {
     public static void main(String[] args) {
         
-        Aluno aluno1 = new Aluno("Efilho", 464654, 5, 6, 7);
-        Aluno aluno2 = new Aluno("Joaone", 40028922, 90, 90, 2);
+        Aluno aluno1 = new Aluno("Pedro", 464654, 5, 6, 7);
+        Aluno aluno2 = new Aluno("Joao", 40028922, 10, 10, 2);
 
         System.out.println(aluno1.nome +" "+ aluno1.matricula + " " + aluno1.nota1 + " " + aluno1.nota2 + " " + aluno1.notatrab);
         aluno1.verificarSituacao();
